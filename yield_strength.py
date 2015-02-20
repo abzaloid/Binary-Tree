6b12e55e627dba762fcf4a7a93644e1e5039ee38
Abzal Serekov <abzaloid@users.noreply.github.com>
2015-02-20 09:49:13 +0600

    added max torque calculations
    changed bugs in conversion

--- yield_strength.py
+++ yield_strength.py
@@ -2,7 +2,7 @@
 gravity = 9.81
 max_mass = 7
 
-power = 1000
+power = 0.001
 
 b1 = 38 * power
 b2 = 83 * power
@@ -15,7 +15,7 @@ wm = 55 * gravity * power
 F = max_mass * gravity
 
 
-a = 37
+a = 37 * power
 y = a / 2
 I = (a**4) / 12
 
@@ -35,3 +35,15 @@ yield_strength = 414
 
 print 'tensile stress =', Gg, 'MPa'
 print 'safety factor', yield_strength / Gg
+
+Tf = F * (h + b3 + l + b2 + l + b1) + \
+	 w * (h / 2 + b3 + l + b2 + l + b1) + \
+	 wm * (b3 + l + b2 + l + b1) + \
+	 wm * (l + b2 + l + b1) + \
+	 w * (l / 2 + b2 + l + b1) + \
+	 wm * (b2 + l + b1) + \
+	 wm * (l + b1) + \
+	 w * (l / 2 + b1) + \
+	 wm * b1
+
+print 'Torque at f is', Tf
